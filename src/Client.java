e037884e1357e263833e585954fea640d9d0c69a
p4sI <pascalpflueger@gmx.de>
2015-04-29 18:04:18 +0200

    stupides geradeaus laufen

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -12,21 +13,32 @@ public class Client {
 	public static void main(String[] args) {
 		String serverIP = args[0];
 		try {
-			NetworkClient networkClient = new NetworkClient(serverIP, "meinTeamName4", ImageIO.read(new File("logo/hellokitty256.png")));
+			NetworkClient networkClient = new NetworkClient(serverIP, "meinTeamName0", ImageIO.read(new File("logo/hellokitty256.png")));
 			
-			networkClient.getMyPlayerNumber();
+			int playerNumber = networkClient.getMyPlayerNumber();
 			networkClient.getTimeLimitInSeconds();
-			
+			int i = 0;
 			while(true){
 				Move move = networkClient.receiveMove();
+				
 				if(move == null){
 					// ich bin dran
 					// zug berechnen
 					// Zeit beachten
 					//new Timer().
-					Move myNextMove = new Move(4, 0, 4, 1);
-					networkClient.sendMove(myNextMove);
 					
+					Move myNextMove = null;
+					if(playerNumber == 0)
+						myNextMove = new Move(4, i, 4, i+1);
+					else if(playerNumber == 1)
+						myNextMove = new Move(7-i, 4, 7-i-1, 4);
+					else if(playerNumber == 2)
+						myNextMove = new Move(4, 7-i, 4, 7-i-1);
+					else if(playerNumber == 3)
+						myNextMove = new Move(i, 4, i+1, 4);
+					
+					networkClient.sendMove(myNextMove);
+					++i;
 				}else{
 					// jemand anders is dran
 				}
