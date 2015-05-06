b3469ae5e33532fbe9340a40aeaf41a264a6699f
p4sI <pascalpflueger@gmx.de>
2015-05-06 12:07:16 +0200

    Board speichern, Rating Funktion

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -10,43 +9,94 @@ import lenz.htw.kimpl.net.NetworkClient;
 
 public class Client {
 
+	static byte[][] board = new byte[8][8];
+	static NetworkClient networkClient;
+	static int playerNumber;
+	static int timeLimit;
+	
 	public static void main(String[] args) {
 		String serverIP = args[0];
+	
 		try {
-			NetworkClient networkClient = new NetworkClient(serverIP, "meinTeamName0", ImageIO.read(new File("logo/hellokitty256.png")));
+			networkClient = new NetworkClient(serverIP, "meinTeamName0", ImageIO.read(new File("logo/hellokitty256.png")));
 			
-			int playerNumber = networkClient.getMyPlayerNumber();
-			networkClient.getTimeLimitInSeconds();
-			int i = 0;
 			while(true){
 				Move move = networkClient.receiveMove();
 				
 				if(move == null){
-					// ich bin dran
-					// zug berechnen
-					// Zeit beachten
-					//new Timer().
-					
-					Move myNextMove = null;
-					if(playerNumber == 0)
-						myNextMove = new Move(4, i, 4, i+1);
-					else if(playerNumber == 1)
-						myNextMove = new Move(7-i, 4, 7-i-1, 4);
-					else if(playerNumber == 2)
-						myNextMove = new Move(4, 7-i, 4, 7-i-1);
-					else if(playerNumber == 3)
-						myNextMove = new Move(i, 4, i+1, 4);
-					
-					networkClient.sendMove(myNextMove);
-					++i;
+					calculateMove();
 				}else{
-					// jemand anders is dran
+					saveMove(move);
 				}
+				printBoard();
 			}
 		} catch (IOException e){
 			throw new RuntimeException("", e);
 			
 		}
+	}
+	
+	public static void init(){
+		playerNumber = networkClient.getMyPlayerNumber();
+		timeLimit = networkClient.getTimeLimitInSeconds();
+		createBoard();
+		printBoard();
+	}
+	
+	public static void createBoard(){
+		for(int i = 1; i < 7; ++i){
+			board[i][0] = 1;
+			board[7][i] = 2;
+			board[i][7] = 3;
+			board[0][i] = 4;
+		}
+	}
+	
+	public static void printBoard(){
+		for(int i = 7; i >= 0; i--){
+			for(int j = 0; j < 8; j++){
+				System.out.print(board[j][i] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void calculateMove(){
+		// ich bin dran
+		// zug berechnen
+		// Zeit beachten
+		// new Timer().
+		
+		Move myNextMove = null;
+		if(playerNumber == 0)
+			myNextMove = new Move(4, 0, 4, 1);
+		else if(playerNumber == 1)
+			myNextMove = new Move(7, 4, 7-1, 4);
+		else if(playerNumber == 2)
+			myNextMove = new Move(4, 7, 4, 7-1);
+		else if(playerNumber == 3)
+			myNextMove = new Move(0, 4, 1, 4);
+		
+		networkClient.sendMove(myNextMove);
+
+	}
+	
+	public static void saveMove(Move move){
+		byte movingPlayer = board[move.fromX][move.fromY];
+		board[move.toX][move.toY] = movingPlayer;				
+		board[move.fromX][move.fromY] = 0;
+	}
+	
+	public byte rate(byte[][] newBoard, byte movingPlayer){
+		byte cnt = 0;
+		for(byte i = 0; i < 8; ++i){
+			for(byte j = 0; j < 8; ++j){
+				if(newBoard[i][j] == movingPlayer)
+					++cnt;
+			}
+		}
+		return cnt;
 	}
 
 }
